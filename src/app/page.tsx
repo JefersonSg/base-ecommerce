5e29d434205eebe31773632ecf47eedbfd4d9a58
jeferson da silva gabri <jefersongabri@gmail.com>
2023-12-14 09:20:19 -0300

    iniciando repozitorio da Base de ecommerces

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image';
+import styles from './page.module.css';
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export default function Home() {
+  return <main className={styles.main}></main>;
+}
