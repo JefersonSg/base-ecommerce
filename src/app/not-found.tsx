29f83ef524344d979871e0214ef5d3c36cf07540
JefersonSg <jefersongabri@gmail.com>
2024-01-19 10:25:14 -0300

    removendo lixo da pagina notfound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import styles from './NotFound.module.css';
-import { Titulo } from '../components/textos/Titulo';
-import { TituloArea } from '../components/textos/TituloArea';
 import { TituloSessao } from '../components/textos/TituloSessao';
 
 const NotFound = () => {
