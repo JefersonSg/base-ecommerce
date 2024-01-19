255d31705b49bfb9aa2a57cab719a680bbec1fe5
JefersonSg <jefersongabri@gmail.com>
2024-01-19 19:38:41 -0300

    Adicinando produtos a pagina de Todos os produtos

---
+++ src/components/produtos/section/Produtos.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import Produto from '../../Produto/Produto';
+import styles from './Produtos.module.css';
+
+const Produtos = () => {
+  return (
+    <div className={styles.produtos}>
+      <Produto
+        nome="Creme Hydra"
+        preco="34,94"
+        promocao={false}
+        img={['produto1']}
+      />
+      <Produto
+        nome="Creme Hydra"
+        preco="34,94"
+        promocao={false}
+        img={['produto1']}
+      />
+      <Produto
+        nome="Creme Hydra"
+        preco="34,94"
+        promocao={false}
+        img={['produto1']}
+      />
+      <Produto
+        nome="Creme Hydra"
+        preco="34,94"
+        promocao={false}
+        img={['produto1']}
+      />
+      <Produto
+        nome="Creme Hydra"
+        preco="34,94"
+        promocao={false}
+        img={['produto1']}
+      />
+      <Produto
+        nome="Creme Hydra"
+        preco="34,94"
+        promocao={false}
+        img={['produto1']}
+      />
+      <Produto
+        nome="Creme Hydra"
+        preco="34,94"
+        promocao={false}
+        img={['produto1']}
+      />
+      <Produto
+        nome="Creme Hydra"
+        preco="34,94"
+        promocao={false}
+        img={['produto1']}
+      />
+      <Produto
+        nome="Creme Hydra"
+        preco="34,94"
+        promocao={false}
+        img={['produto1']}
+      />
+      <Produto
+        nome="Creme Hydra"
+        preco="34,94"
+        promocao={false}
+        img={['produto1']}
+      />
+      <Produto
+        nome="Creme Hydra"
+        preco="34,94"
+        promocao={false}
+        img={['produto1']}
+      />
+      <Produto
+        nome="Creme Hydra"
+        preco="34,94"
+        promocao={false}
+        img={['produto1']}
+      />
+    </div>
+  );
+};
+
+export default Produtos;
--- src/components/produtos/section/SectionProdutos.tsx
+++ src/components/produtos/section/SectionProdutos.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import { Texto } from '../../textos/Texto';
+import styles from './SectionProdutos.module.css';
+import Image from 'next/image';
+import Produtos from './Produtos';
+import BotaoColorido from '../../botoes/BotaoColorido';
 
 const SectionProdutos = () => {
   return (
-    <div>
-      <Texto texto="Todos os produtos" />
+    <div className={styles.section_produtos}>
+      <div className={styles.informacoes}>
+        <Texto texto="Todos os produtos" />
+        <div className={styles.select_view}>
+          <Image
+            alt="imagem de quadrados para mudar a vizualização dos produtos"
+            src={'/produtos/multi_view.svg'}
+            width={17}
+            height={17}
+          />
+
+          <Image
+            alt="imagem de quadrados para mudar a vizualização dos produtos"
+            src={'/produtos/single_view.svg'}
+            width={17}
+            height={17}
+          />
+        </div>
+      </div>
+      <div>
+        <Produtos />
+      </div>
+      <div className={styles.botao}>
+        <BotaoColorido texto="Proximos 12 produtos" />
+      </div>
     </div>
   );
 };
