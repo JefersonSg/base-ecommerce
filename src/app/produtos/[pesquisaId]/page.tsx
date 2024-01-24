99fbf3a8762740c34d2a86de1f5021da0709dd8e
JefersonSg <jefersongabri@gmail.com>
2024-01-24 17:08:56 -0300

    criando botão de filtro personalizado na pagina de pesquisa/todos os produtos

--- src/app/produtos/[pesquisaId]/page.tsx
+++ src/app/produtos/[pesquisaId]/page.tsx
@@ -3,10 +3,12 @@ import styles from './page.module.css';
 import Produtos from '@/src/components/produtos/Produtos';
 
 function page({ params }: { params: { pesquisaId: string } }) {
+  const stringDecoded = decodeURIComponent(params.pesquisaId);
+
   return (
     <div className={styles.produtos_container}>
       <Breadcrumb texto={`Home / Produtos / pesquisa`} />
-      <Produtos pesquisa={params.pesquisaId.replaceAll('%20', ' ')} />
+      <Produtos pesquisa={stringDecoded} />
     </div>
   );
 }
