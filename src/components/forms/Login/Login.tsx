cb906b5c1eac52b5ba33139614ea0e1abe568d94
JefersonSg <jefersongabri@gmail.com>
2024-01-27 08:35:41 -0300

    Corrigindo erro de possivel undefined na mensagem de erro

--- src/components/forms/Login/Login.tsx
+++ src/components/forms/Login/Login.tsx
@@ -1,15 +1,14 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import React, { type FormEvent } from 'react';
+import React from 'react';
 // import axios from 'axios';
 
 import { useForm, type SubmitHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-import Input from '@/src/components/forms/Input';
 
 import styles from './Login.module.css';
 import BotaoRedondo from '../../botoes/BotaoRedondo';
-import { Span } from 'next/dist/trace';
+import Link from 'next/link';
 
 interface Inputs {
   email: string;
@@ -19,33 +18,40 @@ interface Inputs {
 const schema = yup.object({
   email: yup
     .string()
-    .email('Erro, é necessario preencher um email valido')
-    .required('Erro, necerrario preencher o campo de nome'),
+    .email(' É necessario preencher um email valido')
+    .required('É necerrario preencher o campo de Email'),
   password: yup
     .string()
-    .required('Erro, necerrario preencher o campo de senha')
-    .min(8, 'Erro, A senha deve ter  no minimo 8 caracteres')
+    .required('É necerrario preencher o campo de senha')
+    .min(8, ' A senha deve ter  no minimo 8 caracteres')
 });
 
-const Login = () => {
+const Login: React.FC = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors }
   } = useForm({
     resolver: yupResolver(schema)
   });
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => {};
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    console.log(data);
+
+    return true;
+  };
 
   const [error, setError] = React.useState<string | boolean>(false);
 
   React.useEffect(() => {
     setError(false);
     setTimeout(() => {
-      setError(errors?.email?.message);
-      setError(errors?.password?.message);
+      if (errors?.password?.message) {
+        setError(errors?.password?.message);
+      }
+      if (errors?.email?.message) {
+        setError(errors?.email?.message);
+      }
     }, 100);
     const temporizador = setTimeout(function closeError() {
       setError(false);
@@ -58,14 +64,18 @@ const Login = () => {
 
   return (
     <div>
-      <form action="" onSubmit={handleSubmit(onSubmit)}>
+      <form
+        className={styles.form_container}
+        action=""
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className={styles.divInput}>
-          <label htmlFor="email">email</label>
+          <label htmlFor="email">Email</label>
           <input className={styles.input} type="text" {...register('email')} />
           <span className={styles.error}>{errors?.email?.message}</span>
         </div>
         <div className={styles.divInput}>
-          <label htmlFor="password">password</label>
+          <label htmlFor="password">Senha</label>
           <input
             className={styles.input}
             type="text"
@@ -73,7 +83,9 @@ const Login = () => {
           />
           <span className={styles.error}>{errors?.password?.message}</span>
         </div>
-
+        <p className={'texto_indicativo'}>
+          Não possui uma conta? <Link href={'/registrar'}>Cadastre-se</Link>
+        </p>
         <BotaoRedondo texto="Entrar" />
       </form>
       <span className={`${styles.error_span} ${error ? styles.ativo : ''}`}>
--- src/components/forms/registro/RegistrarForm.tsx
+++ src/components/forms/registro/RegistrarForm.tsx
@@ -1,15 +1,14 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import React, { type FormEvent } from 'react';
+import React from 'react';
 // import axios from 'axios';
 
 import { useForm, type SubmitHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-import Input from '@/src/components/forms/Input';
 
-import styles from './Login.module.css';
+import styles from './Registrar.module.css';
 import BotaoRedondo from '../../botoes/BotaoRedondo';
-import { Span } from 'next/dist/trace';
+import Link from 'next/link';
 
 interface Inputs {
   email: string;
@@ -19,19 +18,18 @@ interface Inputs {
 const schema = yup.object({
   email: yup
     .string()
-    .email('Erro, é necessario preencher um email valido')
-    .required('Erro, necerrario preencher o campo de nome'),
+    .email(' É necessario preencher um email valido')
+    .required('É necerrario preencher o campo de Email'),
   password: yup
     .string()
-    .required('Erro, necerrario preencher o campo de senha')
-    .min(8, 'Erro, A senha deve ter  no minimo 8 caracteres')
+    .required('É necerrario preencher o campo de senha')
+    .min(8, ' A senha deve ter  no minimo 8 caracteres')
 });
 
-const Login = () => {
+const RegistrarForm = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors }
   } = useForm({
     resolver: yupResolver(schema)
@@ -44,8 +42,12 @@ const Login = () => {
   React.useEffect(() => {
     setError(false);
     setTimeout(() => {
-      setError(errors?.email?.message);
-      setError(errors?.password?.message);
+      if (errors?.password?.message) {
+        setError(errors?.password?.message);
+      }
+      if (errors?.email?.message) {
+        setError(errors?.email?.message);
+      }
     }, 100);
     const temporizador = setTimeout(function closeError() {
       setError(false);
@@ -58,14 +60,18 @@ const Login = () => {
 
   return (
     <div>
-      <form action="" onSubmit={handleSubmit(onSubmit)}>
+      <form
+        className={styles.form_container}
+        action=""
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className={styles.divInput}>
-          <label htmlFor="email">email</label>
+          <label htmlFor="email">Email</label>
           <input className={styles.input} type="text" {...register('email')} />
           <span className={styles.error}>{errors?.email?.message}</span>
         </div>
         <div className={styles.divInput}>
-          <label htmlFor="password">password</label>
+          <label htmlFor="password">Senha</label>
           <input
             className={styles.input}
             type="text"
@@ -73,7 +79,9 @@ const Login = () => {
           />
           <span className={styles.error}>{errors?.password?.message}</span>
         </div>
-
+        <p className={'texto_indicativo'}>
+          Não possui uma conta? <Link href={'/registrar'}>Cadastre-se</Link>
+        </p>
         <BotaoRedondo texto="Entrar" />
       </form>
       <span className={`${styles.error_span} ${error ? styles.ativo : ''}`}>
@@ -83,4 +91,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default RegistrarForm;
