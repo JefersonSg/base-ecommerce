38ae8112d12eb345ffcb3cb8ab7429e96ce6fe7b
JefersonSg <jefersongabri@gmail.com>
2024-01-28 23:08:30 -0300

    Arrumando tipagens

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,9 @@
 'use client';
 
-import LoginPage from '@/src/components/forms/login/LoginPage';
 import styles from './styles.module.css';
 
 const page = () => {
-  return (
-    <div className={styles.login_container}>
-      <LoginPage />
-    </div>
-  );
+  return <div className={styles.login_container}></div>;
 };
 
 export default page;
