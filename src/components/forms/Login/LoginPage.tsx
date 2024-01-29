c977fdba76436e65a8c58fa8b6c019e608d43ddf
JefersonSg <jefersongabri@gmail.com>
2024-01-28 23:22:59 -0300

    Arrumando tipagens

--- src/components/forms/Login/LoginPage.tsx
+++ src/components/forms/Login/LoginPage.tsx
@@ -1,79 +1,11 @@
-/* eslint-disable @typescript-eslint/no-misused-promises */
 'use client';
 
 import React from 'react';
-// import { Context } from '@/src/shared/context/';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
 
 import styles from './Login.module.css';
-import BotaoRedondo from '@/src/components/botoes/BotaoRedondo';
 import Link from 'next/link';
 
-// interface Inputs {
-//   email: string;
-//   password: string;
-// }
-
-// const schema = yup.object({
-//   email: yup
-//     .string()
-//     .email('É necessário preencher um email válido')
-//     .required('É necessário preencher o campo de Email'),
-//   password: yup
-//     .string()
-//     .required('É necessário preencher o campo de senha')
-//     .min(8, 'A senha deve ter no mínimo 8 caracteres')
-// });
-
 const LoginPage = () => {
-  // const { login } = React.useContext(Context);
-
-  // const {
-  //   register,
-  //   // handleSubmit,
-  //   formState: { errors }
-  // } = useForm({
-  //   resolver: yupResolver(schema)
-  // });
-
-  const [errorMessage, setErrorMessage] = React.useState<string | boolean>(
-    false
-  );
-
-  // const onSubmit: SubmitHandler<Inputs> = async (data) => {
-  //   const dataUser = {
-  //     email: data.email,
-  //     password: data.password
-  //   };
-
-  //   await login(dataUser, setErrorMessage);
-  // };
-
-  // Função de resetar e setar o span de erros
-
-  // handleSubmit(onSubmit)
-
-  // React.useEffect(() => {
-  //   setErrorMessage(false);
-  //   setTimeout(() => {
-  //     if (errors?.password?.message) {
-  //       setErrorMessage(errors?.password?.message);
-  //     }
-  //     if (errors?.email?.message) {
-  //       setErrorMessage(errors?.email?.message);
-  //     }
-  //   }, 100);
-  //   const temporizador = setTimeout(function closeError() {
-  //     setErrorMessage(false);
-  //   }, 5000);
-
-  //   return () => {
-  //     clearTimeout(temporizador);
-  //   };
-  // }, [errors]);
-
   return (
     <div>
       <form
@@ -86,14 +18,7 @@ const LoginPage = () => {
         <h1 className="titulo_sessao">Entre em sua conta</h1>
         <div className={styles.divInput}>
           <label htmlFor="email">Email</label>
-          <input
-            className={styles.input}
-            type="text"
-            id="email"
-            name="email"
-            // {...register('email')}
-          />
-          {/* <span className={styles.error}>{errors?.email?.message}</span> */}
+          <input className={styles.input} type="text" id="email" name="email" />
         </div>
         <div className={styles.divInput}>
           <label htmlFor="password">Senha</label>
@@ -102,21 +27,14 @@ const LoginPage = () => {
             className={styles.input}
             type="password"
             name="password"
-
-            // {...register('password')}
           />
-          {/* <span className={styles.error}>{errors?.password?.message}</span> */}
         </div>
         <p className={'texto_indicativo'}>
           Não possui uma conta? <Link href={'/registrar'}>Cadastre-se</Link>
         </p>
-        <BotaoRedondo texto="Entrar" />
+        <button></button>
       </form>
-      <span
-        className={`${styles.error_span} ${errorMessage ? styles.ativo : ''}`}
-      >
-        {errorMessage}
-      </span>
+      <span></span>
     </div>
   );
 };
