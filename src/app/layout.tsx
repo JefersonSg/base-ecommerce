3d473468844c9a16f49c75b29139799f2a2639c7
JefersonSg <jefersongabri@gmail.com>
2024-02-01 22:02:08 -0300

    Adicionando meta tag para indexação do google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 
 import Providers from '@/src/shared/providers/providers';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,8 +20,22 @@ export const metadata: Metadata = {
   ],
   icons:
     'https://i.pinimg.com/280x280_RS/20/bf/15/20bf15f77c6b9f85b6198a1538a683ca.jpg',
-
-  robots: "'index', 'follow'",
+  verification: {
+    google: 'oZIkI3bhEnXdKiOZst7zIkgD4BW4RLVtYB8jS518PiE'
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1
+    }
+  },
   openGraph: {
     url: 'https://basecommerce.vercel.app/',
     siteName: 'Abayomi Make Beauty',
@@ -39,6 +54,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="oZIkI3bhEnXdKiOZst7zIkgD4BW4RLVtYB8jS518PiE"
+        />
+      </Head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
