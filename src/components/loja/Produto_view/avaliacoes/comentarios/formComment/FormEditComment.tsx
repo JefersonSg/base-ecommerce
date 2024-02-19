8d92bcb0cfc7a184fcfa47d01a02a16657c029fe
JefersonSg <jefersongabri@gmail.com>
2024-02-19 19:47:37 -0300

    removendo console log

--- src/components/loja/Produto_view/avaliacoes/comentarios/formComment/FormEditComment.tsx
+++ src/components/loja/Produto_view/avaliacoes/comentarios/formComment/FormEditComment.tsx
@@ -80,7 +80,6 @@ const FormEditComment = ({
       stars,
       images: data?.images
     };
-    console.log(newDataComment);
     try {
       const response = await updateComment(newDataComment);
 
