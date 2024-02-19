55e42b107cfa703b050bd32654177ecebe4663fb
JefersonSg <jefersongabri@gmail.com>
2024-02-19 18:55:53 -0300

    adicionando suspense API em volda do useSearchParams

--- src/components/loja/Produto_view/avaliacoes/comentarios/formComment/FormComment.tsx
+++ src/components/loja/Produto_view/avaliacoes/comentarios/formComment/FormComment.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 'use client';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import styles from './FormComment.module.css';
 import Estrelas from '../../../../../compartilhado/estrelas/Estrelas';
 import BotaoRedondo from '@/src/components/compartilhado/botoes/BotaoRedondo';
@@ -38,7 +38,6 @@ const FormComment = ({
   });
   const idProduct = useSearchParams().get('_id');
 
-  console.log(idProduct);
   const [imageUrl, setImageUrl] = React.useState<string | ArrayBuffer | null>();
   const watchImage: File[] = watch('images') as File[];
 
@@ -83,88 +82,90 @@ const FormComment = ({
   };
 
   return (
-    <div className={styles.form_comment_container}>
-      <form action="" onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="stars">Avalie o produto</label>
-          <div className={styles.stars}>
-            <input
-              type="radio"
-              name="stars"
-              value={1}
-              onClick={() => {
-                setStars(1);
-              }}
-            />
-            <input
-              type="radio"
-              name="stars"
-              value={2}
-              onClick={() => {
-                setStars(2);
-              }}
-            />
-            <input
-              type="radio"
-              name="stars"
-              value={3}
-              onClick={() => {
-                setStars(3);
-              }}
-            />
-            <input
-              type="radio"
-              name="stars"
-              value={4}
-              onClick={() => {
-                setStars(4);
-              }}
-            />
-            <input
-              type="radio"
-              name="stars"
-              value={5}
-              onClick={() => {
-                setStars(5);
-              }}
-            />
-            <Estrelas stars={stars} type={0} />
-          </div>
-        </div>
-        <div>
-          <label htmlFor="comment">Escreva um comentário</label>
-          <textarea
-            className={styles.textoArea}
-            id="comment"
-            cols={30}
-            rows={10}
-            {...register('comment')}
-          ></textarea>
-        </div>
-        <div>
-          <label htmlFor="images" className={styles.label_foto}>
-            Escolha uma foto
-          </label>
+    <Suspense>
+      <div className={styles.form_comment_container}>
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
           <div>
-            {imageUrl && typeof imageUrl === 'string' && (
-              <Image
-                alt="Imagem upload"
-                src={imageUrl ?? ''}
-                width={50}
-                height={50}
+            <label htmlFor="stars">Avalie o produto</label>
+            <div className={styles.stars}>
+              <input
+                type="radio"
+                name="stars"
+                value={1}
+                onClick={() => {
+                  setStars(1);
+                }}
               />
-            )}
+              <input
+                type="radio"
+                name="stars"
+                value={2}
+                onClick={() => {
+                  setStars(2);
+                }}
+              />
+              <input
+                type="radio"
+                name="stars"
+                value={3}
+                onClick={() => {
+                  setStars(3);
+                }}
+              />
+              <input
+                type="radio"
+                name="stars"
+                value={4}
+                onClick={() => {
+                  setStars(4);
+                }}
+              />
+              <input
+                type="radio"
+                name="stars"
+                value={5}
+                onClick={() => {
+                  setStars(5);
+                }}
+              />
+              <Estrelas stars={stars} type={0} />
+            </div>
           </div>
-          <input
-            type="file"
-            id="images"
-            style={{ display: 'none' }}
-            {...register('images')}
-          />
-        </div>
-        <BotaoRedondo texto="enviar" />
-      </form>
-    </div>
+          <div>
+            <label htmlFor="comment">Escreva um comentário</label>
+            <textarea
+              className={styles.textoArea}
+              id="comment"
+              cols={30}
+              rows={10}
+              {...register('comment')}
+            ></textarea>
+          </div>
+          <div>
+            <label htmlFor="images" className={styles.label_foto}>
+              Escolha uma foto
+            </label>
+            <div>
+              {imageUrl && typeof imageUrl === 'string' && (
+                <Image
+                  alt="Imagem upload"
+                  src={imageUrl ?? ''}
+                  width={50}
+                  height={50}
+                />
+              )}
+            </div>
+            <input
+              type="file"
+              id="images"
+              style={{ display: 'none' }}
+              {...register('images')}
+            />
+          </div>
+          <BotaoRedondo texto="enviar" />
+        </form>
+      </div>
+    </Suspense>
   );
 };
 
