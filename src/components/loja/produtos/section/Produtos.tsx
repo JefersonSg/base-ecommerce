1cafbcd250724b92e7c1ede3b1b3843a2d84a414
JefersonSg <jefersongabri@gmail.com>
2024-03-13 19:31:38 -0300

    removendo item nao usado

--- src/components/loja/produtos/section/Produtos.tsx
+++ src/components/loja/produtos/section/Produtos.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Produto from '@/src/components/loja/card-product/Produto';
 
-import styles from './Produtos.module.css';
 import { useQuery } from '@tanstack/react-query';
 import { getAllProducts } from '@/src/shared/api/GETS';
 import { type ProductApi } from '@/src/shared/helpers/interfaces';
