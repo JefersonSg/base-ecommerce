9268043deb96c68bd038cb6115e9aab196bb2cc0
JefersonSg <jefersongabri@gmail.com>
2024-03-15 19:23:07 -0300

    ajustando pagina notfound para os novos fetchs

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,11 +2,28 @@ import React from 'react';
 import styles from './NotFound.module.css';
 import { Footer } from '../components/loja/Footer/Footer';
 import { Header } from '../components/loja/Header/Header';
+import { type subcategoriesListByCategory } from './(loja)/layout';
+import { getAllCategories, getSubcategoryByCategory } from '../shared/api/GETS';
 
-const NotFound = () => {
+const NotFound = async () => {
+  const categories = await getAllCategories();
+
+  const subcategoriesList: subcategoriesListByCategory = [];
+
+  const getSubcategoriesList = async () => {
+    let i = 0;
+    for (const category of categories.categories) {
+      const subcategory = await getSubcategoryByCategory(category._id);
+
+      subcategoriesList[i] = subcategory;
+      i++;
+    }
+  };
+
+  await getSubcategoriesList();
   return (
     <>
-      <Header />
+      <Header categories={categories} subcategoriesList={subcategoriesList} />
       <div className={styles.notFound}>
         <h1 className="titulo_sessao">404 - Página não encontrada</h1>
       </div>
