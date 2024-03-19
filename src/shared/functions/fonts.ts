e734b43716aaab1a30a1421388bbd04448a81d30
JefersonSg <jefersongabri@gmail.com>
2024-03-19 18:27:07 -0300

    Adicionando link ao nav ao inves de um P, adicionando Font direta ao dashboard

--- src/shared/functions/fonts.ts
+++ src/shared/functions/fonts.ts
@@ -1,8 +1,15 @@
-import { Montserrat } from 'next/font/google';
+import { Montserrat, Public_Sans } from 'next/font/google';
 
 export const typeFirst = Montserrat({
   weight: ['200', '300', '400', '500', '600', '700'],
   subsets: ['latin'],
   display: 'auto',
   variable: '--type-first-montserrat'
+});
+
+export const typeFirstDashboard = Public_Sans({
+  weight: ['200', '300', '400', '500', '600', '700'],
+  subsets: ['latin'],
+  display: 'auto',
+  variable: '--type-first-Dashboard'
 });
