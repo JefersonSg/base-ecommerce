b6be3b011f62cd8740e3f421294fcf9470960369
JefersonSg <jefersongabri@gmail.com>
2024-04-11 13:38:12 -0300

    ajustando erro

--- src/app/dashboard/produtos/copiar/page.tsx
+++ src/app/dashboard/produtos/copiar/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import BreadcrumbDashboard from '@/src/components/dashboard/breadcrumb/BreadcrumbDashboard';
-import BodyCopyForm from '@/src/components/dashboard/produtos-lista/copiar/BodyCopyForm';
+import BodyCopyForm from '@/src/components/dashboard/produtos-lista/copiar/BodyEditForm';
 
 const page = () => {
   return (
