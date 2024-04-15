101c8640dfef5b56e4c7a3e0c8d143a8df504c13
JefersonSg <jefersongabri@gmail.com>
2024-04-15 20:35:20 -0300

    removendo pagina de copia

--- src/app/dashboard/produtos/copiar/page.tsx
+++ src/app/dashboard/produtos/copiar/page.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import BreadcrumbDashboard from '@/src/components/dashboard/breadcrumb/BreadcrumbDashboard';
-import BodyCopyForm from '@/src/components/dashboard/produtos-lista/copiar/BodyEditForm';
 
 const page = () => {
   return (
     <main className="dashboard_container">
       <div className={`container_dashboard `}>
         <BreadcrumbDashboard text="Update" />
-        <BodyCopyForm />
       </div>
     </main>
   );
