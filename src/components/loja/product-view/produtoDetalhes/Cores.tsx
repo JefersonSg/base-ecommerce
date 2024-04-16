b798f02c0a8ac4988bf55d5fa56911743a10627c
JefersonSg <jefersongabri@gmail.com>
2024-04-16 14:45:16 -0300

    removendo visualização da cor caso não haja uma cor escrita

--- src/components/loja/product-view/produtoDetalhes/Cores.tsx
+++ src/components/loja/product-view/produtoDetalhes/Cores.tsx
@@ -21,62 +21,72 @@ function Cores({
     codeColors ? codeColors[0] ?? '' : ''
   );
   return (
-    <div className={styles.selectColor_container}>
-      <div
-        className={styles.cores}
-        onClick={() => {
-          setAtivo(!ativo);
-        }}
-      >
-        <p className="texto">{colorSelected}</p>
-        <div className={styles.select_color}>
+    <>
+      {colors?.[0].length > 0 ? (
+        <div className={styles.selectColor_container}>
           <div
-            className={styles.cor_selecionada}
-            style={{ background: codeColorSelected }}
-          ></div>
-          <Image
-            className={`${ativo ? styles.ativo : ''} ${styles.seta}`}
-            alt="Seta"
-            src={'/setaBaixo.svg'}
-            width={9}
-            height={9}
-          />
-        </div>
-      </div>
-      <p className={styles.cor_selecionada_span}>
-        Cor: <span>{colorSelected}</span>
-      </p>
-      <div className={`${styles.selectColors} ${ativo ? styles.ativo : ''}`}>
-        {colors?.map((color, index) => {
-          return (
-            <div
-              key={index}
-              className={styles.cores_disponiveis}
-              onClick={() => {
-                setColorSelected(colors[index]);
-                setCodeColorSelected(codeColors?.[index] ?? '');
-                setAtivo(false);
-              }}
-            >
-              {color}
+            className={styles.cores}
+            onClick={() => {
+              setAtivo(!ativo);
+            }}
+          >
+            <p className="texto">{colorSelected}</p>
+            <div className={styles.select_color}>
               <div
-                className={`${styles.cor_selecionada} ${
-                  codeColorSelected === codeColors?.[index] ? styles.ativo : ''
-                }`}
-                style={{
-                  background: codeColors?.[index],
-                  border: `${
-                    codeColorSelected === codeColors?.[index]
-                      ? '1.5px solid' + codeColors[index]
-                      : ''
-                  }`
-                }}
+                className={styles.cor_selecionada}
+                style={{ background: codeColorSelected }}
               ></div>
+              <Image
+                className={`${ativo ? styles.ativo : ''} ${styles.seta}`}
+                alt="Seta"
+                src={'/setaBaixo.svg'}
+                width={9}
+                height={9}
+              />
             </div>
-          );
-        })}
-      </div>
-    </div>
+          </div>
+          <p className={styles.cor_selecionada_span}>
+            Cor: <span>{colorSelected}</span>
+          </p>
+          <div
+            className={`${styles.selectColors} ${ativo ? styles.ativo : ''}`}
+          >
+            {colors?.map((color, index) => {
+              return (
+                <div
+                  key={index}
+                  className={styles.cores_disponiveis}
+                  onClick={() => {
+                    setColorSelected(colors[index]);
+                    setCodeColorSelected(codeColors?.[index] ?? '');
+                    setAtivo(false);
+                  }}
+                >
+                  {color}
+                  <div
+                    className={`${styles.cor_selecionada} ${
+                      codeColorSelected === codeColors?.[index]
+                        ? styles.ativo
+                        : ''
+                    }`}
+                    style={{
+                      background: codeColors?.[index],
+                      border: `${
+                        codeColorSelected === codeColors?.[index]
+                          ? '1.5px solid' + codeColors[index]
+                          : ''
+                      }`
+                    }}
+                  ></div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ) : (
+        ''
+      )}
+    </>
   );
 }
 
