0d45c5cb3ec199e1a979b275015975bf89e61818
JefersonSg <jefersongabri@gmail.com>
2024-04-19 12:23:26 -0300

    adicionando google tag manager, alterando nome e descrições do site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,25 @@
 import type { Metadata } from 'next';
 import { typeFirst, typeFirstDashboard } from '../shared/functions/fonts';
 import './globals.css';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 import Providers from '@/src/shared/providers/providers';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
-  title: 'Abayomi Make Beauty',
+  title: 'Loja Mayse | Moda intima - Compre e Receba em Casa',
   description:
-    'Aqui na Abayomi Make Beauty você encontra uma variedade enorme de produtos selecionados com a melhor qualidade do mercado',
+    'Bem-vindas à Loja Mayse, seu destino para moda íntima que realça sua feminilidade e desperta seus desejos. Oferecemos uma variedade de lingeries cuidadosamente selecionadas, além de produtos sensuais imperdíveis. Explore sua sensualidade na Loja Mayse.',
   keywords: [
-    'Abayomi Make Beauty',
-    'Maquiagem',
-    'Maquiagens de qualidade',
-    'Make Beauty'
+    'Loja',
+    'Mayse',
+    'Loja Mayse',
+    'sexy shop',
+    'lingerie',
+    'calcinha',
+    'sutiã',
+    'conjuntos sexy',
+    'loja de sexy shop barata'
   ],
   icons:
     'https://i.pinimg.com/280x280_RS/20/bf/15/20bf15f77c6b9f85b6198a1538a683ca.jpg',
@@ -33,13 +40,13 @@ export const metadata: Metadata = {
     }
   },
   openGraph: {
-    url: 'https://basecommerce.vercel.app/',
-    siteName: 'Abayomi Make Beauty',
-    title: 'Abayomi Make Beauty',
+    url: 'https://lojamayse.com/',
+    siteName: 'Loja Mayse | Moda intima - Compre e Receba em Casa',
+    title: 'Loja Mayse | Moda intima - Compre e Receba em Casa',
     description:
-      'Aqui na Abayomi Make Beauty você encontra uma variedade enorme de produtos selecionados com a melhor qualidade do mercado',
+      'Bem-vindas à Loja Mayse, seu destino para moda íntima que realça sua feminilidade e desperta seus desejos. Oferecemos uma variedade de lingeries cuidadosamente selecionadas, além de produtos sensuais imperdíveis. Explore sua sensualidade na Loja Mayse.',
     images:
-      'https://i.pinimg.com/280x280_RS/20/bf/15/20bf15f77c6b9f85b6198a1538a683ca.jpg'
+      'https://drive.google.com/uc?export=view&id=1RD-W1nNYdiYwvYj_4vdM3QE5Qf2Xe1t7'
   }
 };
 
@@ -53,6 +60,7 @@ export default function RootLayout({
       <body className={`${typeFirst.variable} ${typeFirstDashboard.variable}`}>
         <Providers>{children}</Providers>
       </body>
+      <GoogleTagManager gtmId="G-NYKP51D935" />
     </html>
   );
 }
