ac0feadf22ec1e8e2750f43be2c1e886c6ab05e1
JefersonSg <jefersongabri@gmail.com>
2024-04-22 14:14:08 -0300

    merge main

--- src/components/loja/product-view/interacoesUser/Interacoes.tsx
+++ src/components/loja/product-view/interacoesUser/Interacoes.tsx
@@ -4,21 +4,17 @@ import Compartilhar from './Compartilhar';
 import Update from './Update';
 import React from 'react';
 import EstrelasProduto from './EstrelasProduto';
-import InteracoesUserModalCompartilhar from './compartilhar/modal-compartilhar';
 
 async function Interacoes({ id, stars }: { id: string; stars: number }) {
   return (
-    <>
-      <div className={styles.interatividades}>
-        <EstrelasProduto stars={stars} />
-        <div className={styles.interacao}>
-          <Update id={id} />
-          <Favotiro productId={id} />
-          <Compartilhar />
-        </div>
+    <div className={styles.interatividades}>
+      <EstrelasProduto stars={stars} />
+      <div className={styles.interacao}>
+        <Update id={id} />
+        <Favotiro productId={id} />
+        <Compartilhar />
       </div>
-      <InteracoesUserModalCompartilhar />
-    </>
+    </div>
   );
 }
 
--- src/components/loja/product-view/interacoesUser/compartilhar/modal-compartilhar.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './modal.module.css';
-
-export default function InteracoesUserModalCompartilhar() {
-  return (
-    <div className={styles.compartilhar_container}>
-      <h1>Compartilhe no WhatsApp</h1>
-    </div>
-  );
-}
