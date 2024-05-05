dc54578b71596c931ed32ea136d4ecadab450614
JefersonSg <jefersongabri@gmail.com>
2024-05-05 16:06:20 -0300

    testando tag manager do google

--- src/app/(loja)/layout.tsx
+++ src/app/(loja)/layout.tsx
@@ -5,7 +5,7 @@ import { BtnZap } from '@/src/components/compartilhado/botoes/btnZap/BtnZap';
 import { type subcategoryInterface } from '@/src/shared/helpers/interfaces';
 import HeaderContainer from '@/src/components/loja/Header/header-container';
 import './globalsLoja.css';
-import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 export type subcategoriesListByCategory = Record<
   number,
