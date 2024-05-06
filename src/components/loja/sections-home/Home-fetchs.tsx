2eaea1f1766923a5a4ee9d9ee89bb063c470bb87
JefersonSg <jefersongabri@gmail.com>
2024-05-06 09:36:00 -0300

    adicionando campo de promoções

--- src/components/loja/sections-home/Home-fetchs.tsx
+++ src/components/loja/sections-home/Home-fetchs.tsx
@@ -4,12 +4,17 @@ import Categorias from '@/src/components/loja/categorias/Categorias';
 import {
   getAllActiveProducts,
   getAllCategories,
+  getProductByPromotion,
   getProductBySales
 } from '@/src/shared/api/GETS';
+import { type ProductApi } from '@/src/shared/helpers/interfaces';
 
 export default async function HomeFetchs() {
   const novidades = await getAllActiveProducts();
   const maisVendidos = await getProductBySales();
+  const promocoes = (await getProductByPromotion()) as {
+    products: ProductApi[];
+  };
   const categorias = await getAllCategories();
 
   return (
@@ -17,16 +22,55 @@ export default async function HomeFetchs() {
       <Suspense>
         <Categorias categorias={categorias} />
       </Suspense>
-      <Suspense>
-        <Section data={novidades} nomeSessao="Novidades" link={'novidades'} />
-      </Suspense>
-      <Suspense>
-        <Section
-          data={maisVendidos}
-          nomeSessao="Mais vendidos"
-          link={'mais-vendidos'}
-        />
-      </Suspense>
+      {promocoes?.products?.length > 3 ? (
+        <>
+          <Suspense>
+            <Section
+              data={promocoes}
+              nomeSessao="Promoções"
+              link={'promocoes'}
+            />
+          </Suspense>
+          <Suspense>
+            <Section
+              data={novidades}
+              nomeSessao="Novidades"
+              link={'novidades'}
+            />
+          </Suspense>
+          <Suspense>
+            <Section
+              data={maisVendidos}
+              nomeSessao="Mais vendidos"
+              link={'mais-vendidos'}
+            />
+          </Suspense>
+        </>
+      ) : (
+        <>
+          <Suspense>
+            <Section
+              data={novidades}
+              nomeSessao="Novidades"
+              link={'novidades'}
+            />
+          </Suspense>
+          <Suspense>
+            <Section
+              data={maisVendidos}
+              nomeSessao="Mais vendidos"
+              link={'mais-vendidos'}
+            />
+          </Suspense>
+          <Suspense>
+            <Section
+              data={promocoes}
+              nomeSessao="Promoções"
+              link={'promocoes'}
+            />
+          </Suspense>
+        </>
+      )}
     </>
   );
 }
