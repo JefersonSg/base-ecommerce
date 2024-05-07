9a29f3e70e3e764be0090a0f41b1e47bb04e0f74
JefersonSg <jefersongabri@gmail.com>
2024-05-07 20:01:00 -0300

    ajustes

--- src/app/(loja)/layout.tsx
+++ src/app/(loja)/layout.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Footer } from '@/src/components/loja/Footer/Footer';
 import { BtnZap } from '@/src/components/compartilhado/botoes/btnZap/BtnZap';
 
-import HeaderContainer from '@/src/components/loja/Header/header-fetchs';
 import './globalsLoja.css';
+import HeaderFetchs from '@/src/components/loja/Header/HeaderFetchs';
 
 export default async function LojaLayout({
   children
@@ -12,7 +12,7 @@ export default async function LojaLayout({
 }) {
   return (
     <>
-      <HeaderContainer />
+      <HeaderFetchs />
       {children}
       <BtnZap />
       <Footer />
