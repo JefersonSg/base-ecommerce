604fb3a3892e8dd58cf5aa1e98724a3eee068589
Jeferson Gabri <jefersongabri@gmail.com>
2025-06-14 16:59:57 -0300

    aumentando o tamanho da foto do product view provisóriamente para alterar o layout

--- src/components/dashboard/Header/Header.tsx
+++ src/components/dashboard/Header/Header.tsx
@@ -34,7 +34,7 @@ export function Header() {
           <Link href={'/'} className={`${styles.logo} ${styles.mobile}`}>
             <Image
               alt="Logo"
-              src={'/header/dashboard/Logo.svg'}
+              src={'/header/dashboard/logo.svg'}
               width={100}
               height={65}
             />
